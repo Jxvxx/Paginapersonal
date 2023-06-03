e41ea678502158ce82004bffd506a115d8686d29
Jxvxx <ffffffffffffqlll@gmail.com>
2023-06-03 03:18:30 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {HeroLayout1, QuestionCardCollection} from "./ui-components";
+
+function App() {
+  return (
+    <div className='App'>
+      <HeroLayout1 width={"100%"}/>
+      <QuestionCardCollection/>
+    </div>
+  );
+}
+
+export default App;
